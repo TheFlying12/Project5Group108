dd60fe971295f2ca5e97e46c98ce9c240efded4c
Pstock7 <58706544+Pstock7@users.noreply.github.com>
2021-04-23 14:45:52 -0400

    Race class

---
+++ src/Race.java
@@ -0,0 +1,91 @@
+package backend;
+
+/**
+ * This class represents a Race object which has a name and numbers for infected
+ * and dead people
+ * 
+ * @author Patrick Stock (pstock)
+ * @version 4/18/2021
+ */
+public class Race {
+
+    private String name;
+    private int infected;
+    private int deaths;
+
+    /**
+     * Creates a new Race object
+     * 
+     * @param n
+     *            name
+     * @param inf
+     *            infected people
+     * @param de
+     *            dead people
+     */
+    public Race(String n, int inf, int de) {
+        name = n;
+        infected = inf;
+        deaths = de;
+    }
+
+
+    /**
+     * Getter method for the name
+     * 
+     * @return the name of the race
+     */
+    public String getName() {
+        return name;
+    }
+
+
+    /**
+     * Getter method for the infected people
+     * 
+     * @return the number of infected people
+     */
+    public int getInfected() {
+        return infected;
+    }
+
+
+    /**
+     * Getter method for the dead people
+     * 
+     * @return the number of dead people
+     */
+    public int getDeaths() {
+        return deaths;
+    }
+
+
+    /**
+     * Gets a ratio of the deaths and infected as a floating point number
+     * 
+     * @return the ratio of dead people to infected people
+     */
+    public float getRatio() {
+        return (float)deaths / (float)infected;
+    }
+
+
+    /**
+     * Allows the comparison of two races based on their ratios
+     * 
+     * @param other
+     *            Race object to compare to
+     * @return 1 if greater than, 0 if the same, -1 if less than
+     */
+    public int compareTo(Race other) {
+        if (this.getRatio() > other.getRatio()) {
+            return 1;
+        }
+        else if (this.getRatio() == other.getRatio()) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+}
---
+++ src/RaceTest.java
@@ -0,0 +1,63 @@
+package backend;
+
+import student.TestCase;
+
+/**
+ * This class tests all of the methods in the Race class
+ * 
+ * @author Patrick Stock (pstock)
+ * @version 4/18/2021
+ */
+public class RaceTest extends TestCase {
+
+    private Race white;
+
+    /**
+     * Sets up the objects for testing
+     */
+    public void setUp() {
+        white = new Race("White", 100, 10);
+    }
+
+
+    /**
+     * Tests the getName method
+     */
+    public void testGetName() {
+        assertEquals("White", white.getName());
+    }
+
+
+    /**
+     * Tests the getInfected method
+     */
+    public void testGetInfected() {
+        assertEquals(100, white.getInfected());
+    }
+
+
+    /**
+     * Tests the getDeaths method
+     */
+    public void testGetDeaths() {
+        assertEquals(10, white.getDeaths());
+    }
+
+
+    /**
+     * Tests the getRatio method
+     */
+    public void testGetRatio() {
+        assertEquals(0.1, white.getRatio(), 0.001);
+    }
+
+
+    /**
+     * Tests the compareTo method
+     */
+    public void testCompareTo() {
+        assertEquals(1, white.compareTo(new Race("Black", 100, 5)));
+        assertEquals(0, white.compareTo(new Race("Asian", 100, 10)));
+        assertEquals(-1, white.compareTo(new Race("Hispanic", 100, 15)));
+    }
+}
